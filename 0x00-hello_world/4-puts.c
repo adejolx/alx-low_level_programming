787eef21d999e13775455c9603eef0bfcaead816
adejolx <dejoladaniel@gmail.com>
2023-12-23 03:09:33 +0100

    feat: add a c program that prints a string to stdout
    Printed string is 'Programming is like building a multilingual puzzle'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: writes a string to stdout with the puts function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
