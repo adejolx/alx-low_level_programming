5d9f51484da0d200cf4a8f5aca20b0ef7edcaeaf
adejolx <dejoladaniel@gmail.com>
2023-12-24 01:34:35 +0100

    feat: add program that prints uppercase and lowercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase and then in uppercase
+ * followed by a new line
+ * Description: increments the ascii code to print successive letters
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int lower_case_begin = 65;
+	int lower_case_end = 90;
+	int upper_case_begin = 97;
+	int upper_case_end = 122;
+
+	while (lower_case_begin <= lower_case_end)
+	{
+		putchar(lower_case_begin);
+		lower_case_begin++;
+	}
+
+	while (upper_case_begin <= upper_case_end)
+	{
+		putchar(upper_case_begin);
+		upper_case_begin++;
+	}
+		putchar(10);
+
+	return (0);
+}
