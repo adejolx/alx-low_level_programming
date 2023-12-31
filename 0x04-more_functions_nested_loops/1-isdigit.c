96bdf4a6475737a9499ad618138a818b2a3c77cf
adejolx <dejoladaniel@gmail.com>
2023-12-31 19:55:29 +0100

    feat: check for digit (0 - 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for digit (0 - 9)
+ * @c: The digit to be checked
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int digit_begin = 48, digit_end = 57, i = digit_begin;
+
+	while (i >= digit_begin && i <= digit_end)
+	{
+		if (i == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
