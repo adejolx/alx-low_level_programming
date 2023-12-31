a038c4f8397cfd87f28c8c85eb22c815d7065618
adejolx <dejoladaniel@gmail.com>
2023-12-31 12:41:34 +0100

    feat: print the first 98 numbers of the fib sequence
    This solution treats overflow specially without resorting to the long or long long types or utilizing an external lib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 fibonacci numbers
+ * starting with 1 and 2, followed by a new line
+ * Description: Because a fib sequence will eventually lead
+ * to type overflow, the overflow denoted by *_over is computed
+ * and concatenated with the non-overflow part. note that 10^9
+ * used as the ceiling is arbitrarily large enough to cater for
+ * the overflow requirements
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	unsigned int i, n = 98, _ceil = 1000000000,
+	sum_over, f1_over = 0, f2_over = 0,
+	sum, f1 = 1, f2 = 2;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (f1_over > 0)
+			printf("%u", f1_over);
+
+		printf("%u", f1);
+
+		sum_over = f1_over + f2_over + (f1 + f2) / _ceil;
+		f1_over = f2_over;
+		f2_over = sum_over;
+
+		sum = (f1 + f2) % _ceil;
+		f1 = f2;
+		f2 = sum;
+
+		if (i != 98)
+			printf(", ");
+		if (i % 10 == 0)
+			printf("\n");
+	}
+
+	printf("\n");
+	return (0);
+}
