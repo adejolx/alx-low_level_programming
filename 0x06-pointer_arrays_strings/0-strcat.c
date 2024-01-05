15539c1f56205b00ceb1f8584459319f2c859644
adejolx <dejoladaniel@gmail.com>
2024-01-05 06:23:40 +0100

    feat: concatenate two strings

---
+++ 0x06-pointer_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+* @dest: The string to be concatenated to
+* @src: The string to be concatenated from
+* Return: The result of the concatenation
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len_d = 0, len_s = 0, i = 0;
+
+	while (*(dest + len_d))
+		len_d++;
+
+	while (*(src + len_s))
+		len_s++;
+
+	while (i <= len_s)
+	{
+		*(dest + len_d + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
---
+++ 0x06-pointer_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN
+#define MAIN
+void _putchar(char c);
+char *_strcat(char *dest, char *src);
+#endif
